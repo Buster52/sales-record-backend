3e7cc1901c551a527d6027afc9209b2b95c20ef6
Ed <e.gonzalez@criptext.com>
2021-06-16 16:19:07 -0500

    Enable async

--- src/main/java/com/buster/backend/SalesRecordApplication.java
+++ src/main/java/com/buster/backend/SalesRecordApplication.java
@@ -2,12 +2,14 @@ package com.buster.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SalesRecordApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesRecordApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalesRecordApplication.class, args);
+    }
 
 }
