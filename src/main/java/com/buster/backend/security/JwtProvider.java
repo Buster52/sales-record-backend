6a6429ae3bd7e8d1988330e18d8af7993e7d07b0
Ed <e.gonzalez@criptext.com>
2021-06-16 16:26:23 -0500

    jwt configurations

---
+++ src/main/java/com/buster/backend/security/JwtProvider.java
@@ -0,0 +1,54 @@
+package com.buster.backend.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class JwtProvider {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration.time}")
+    private Long jwtExpirationInMills;
+
+    @PostConstruct
+    protected void init() {
+        secret = Base64.getEncoder().encodeToString(secret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        org.springframework.security.core.userdetails.User principal =
+                (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMills)))
+                .compact();
+    }
+
+    public boolean validateToken(String jwt) {
+        Jwts.parserBuilder()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(jwt);
+        return true;
+    }
+
+    public String getUsernameFromjwt(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+}
