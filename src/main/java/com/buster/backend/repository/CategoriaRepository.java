e19d1fca4bd30e5d3e448db3036bcad31773b293
Ed <e.gonzalez@criptext.com>
2021-06-27 15:49:59 -0500

    methods added

--- src/main/java/com/buster/backend/repository/CategoriaRepository.java
+++ src/main/java/com/buster/backend/repository/CategoriaRepository.java
@@ -2,6 +2,11 @@ package com.buster.backend.repository;
 
 import com.buster.backend.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Optional<Categoria> findByName(String category);
 }
--- src/main/java/com/buster/backend/repository/ProductRepository.java
+++ src/main/java/com/buster/backend/repository/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.buster.backend.repository;
 
+import com.buster.backend.model.Categoria;
 import com.buster.backend.model.Producto;
+import com.buster.backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Producto, Long> {
+    List<Producto> findAllByCategoria(Categoria categoria);
+
+    List<Producto> findByUsuario(Usuario usuario);
 }
