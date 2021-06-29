ec6e967b94c3df9fecb887cb7900fe73819138c0
Ed <e.gonzalez@criptext.com>
2021-06-28 20:29:21 -0500

    improving mapper

--- src/main/java/com/buster/backend/mapper/CategoriaMapper.java
+++ src/main/java/com/buster/backend/mapper/CategoriaMapper.java
@@ -1,9 +1,10 @@
 package com.buster.backend.mapper;
 
-import com.buster.backend.dto.CategoriaDTO;
+import com.buster.backend.dto.CategoryRequest;
+import com.buster.backend.dto.CategoryResponse;
 import com.buster.backend.model.Categoria;
 import com.buster.backend.model.Producto;
-import org.mapstruct.InheritInverseConfiguration;
+import com.buster.backend.model.Usuario;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -12,14 +13,18 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface CategoriaMapper {
 
+    @Mapping(target = "usuario", source = "usuario")
+    @Mapping(target = "name", source = "categoryRequest.name")
+    @Mapping(target = "products", ignore = true)
+    Categoria map(CategoryRequest categoryRequest, Usuario usuario);
+
+    @Mapping(target = "id", source = "categoryId")
+    @Mapping(target = "categoryName", source = "name")
     @Mapping(target = "numberOfProducts", expression = "java(mapProducts(categoria.getProducts()))")
-    CategoriaDTO toDto(Categoria categoria);
+    @Mapping(target = "userName", source = "usuario.username")
+    CategoryResponse mapToDto(Categoria categoria);
 
     default Integer mapProducts(List<Producto> numberOfProducts) {
         return numberOfProducts.size();
     }
-
-    @InheritInverseConfiguration
-    @Mapping(target = "products", ignore = true)
-    Categoria fromDto(CategoriaDTO categoriaDTO);
 }
