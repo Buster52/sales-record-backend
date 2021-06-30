76dac21304f36486611595af770b18767a84cabd
Ed <e.gonzalez@criptext.com>
2021-06-29 19:31:43 -0500

    improve

--- src/main/java/com/buster/backend/mapper/CategoriaMapper.java
+++ src/main/java/com/buster/backend/mapper/CategoriaMapper.java
@@ -3,28 +3,28 @@ package com.buster.backend.mapper;
 import com.buster.backend.dto.CategoryRequest;
 import com.buster.backend.dto.CategoryResponse;
 import com.buster.backend.model.Categoria;
-import com.buster.backend.model.Producto;
 import com.buster.backend.model.Usuario;
+import com.buster.backend.repository.ProductRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface CategoriaMapper {
+public abstract class CategoriaMapper {
+    @Autowired
+    private ProductRepository productRepository;
 
     @Mapping(target = "usuario", source = "usuario")
     @Mapping(target = "name", source = "categoryRequest.name")
-    @Mapping(target = "products", ignore = true)
-    Categoria map(CategoryRequest categoryRequest, Usuario usuario);
+    public abstract Categoria map(CategoryRequest categoryRequest, Usuario usuario);
 
     @Mapping(target = "id", source = "categoryId")
     @Mapping(target = "categoryName", source = "name")
-    @Mapping(target = "numberOfProducts", expression = "java(mapProducts(categoria.getProducts()))")
+    @Mapping(target = "numberOfProducts", expression = "java(mapProducts(categoria))")
     @Mapping(target = "userName", source = "usuario.username")
-    CategoryResponse mapToDto(Categoria categoria);
+    public abstract CategoryResponse mapToDto(Categoria categoria);
 
-    default Integer mapProducts(List<Producto> numberOfProducts) {
-        return numberOfProducts.size();
+    Integer mapProducts(Categoria categoria) {
+        return productRepository.findAllByCategoria(categoria).size();
     }
 }
