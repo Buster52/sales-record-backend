4a0572348c8df2a10ca5b92f902a657cba2e8e5c
buster <e.gonzalez@criptext.com>
2021-07-23 19:31:32 -0500

    refactor: fix bug and improve methods

--- src/main/java/com/buster/backend/service/VentaService.java
+++ src/main/java/com/buster/backend/service/VentaService.java
@@ -2,6 +2,7 @@ package com.buster.backend.service;
 
 import com.buster.backend.dto.VentaRequest;
 import com.buster.backend.dto.VentaResponse;
+import com.buster.backend.exceptions.CustomException;
 import com.buster.backend.exceptions.NotFoundException;
 import com.buster.backend.mapper.VentaMapper;
 import com.buster.backend.model.Producto;
@@ -33,12 +34,14 @@ public class VentaService {
         if (producto.isEmpty()) {
             throw new NotFoundException("No existe ese producto.");
         }
-        if (producto.get().getAmount() > 0) {
-            Venta venta = ventaRepository.save(ventaMapper.map(ventaRequest, producto.get(), authService.getCurrentUser()));
-            ventaRequest.setSaleId(venta.getSaleId());
+        if (producto.get().getAmount() < 1) {
+            throw new CustomException("No hay unidades disponibles");
+        } else {
+            Venta ventaMapped = ventaMapper.map(ventaRequest, producto.get(), authService.getCurrentUser());
+            ventaRepository.save(ventaMapped);
+            producto.get().setAmount(producto.get().getAmount() - ventaRequest.getAmount());
+            productRepository.save(producto.get());
         }
-        producto.get().setAmount(producto.get().getAmount() - ventaRequest.getAmount());
-        productRepository.save(producto.get());
     }
 
     @Transactional(readOnly = true)
@@ -50,17 +53,17 @@ public class VentaService {
     }
 
     @Transactional
-    public void updatePay(VentaRequest ventaRequest, Long id) {
+    public void updatePay(Double pay, Long id) {
         Venta venta = ventaRepository.findById(id).orElseThrow(
                 () -> new NotFoundException("No existe venta con id - " + id)
         );
-        Optional<Producto> producto = productRepository.findByName(ventaRequest.getProductName());
-        if (producto.isPresent()) {
-            Double totalPay = venta.getPay() + ventaRequest.getPay();
-            Double balance = venta.getBalance() - ventaRequest.getPay();
-            venta.setPay(totalPay);
-            venta.setBalance(balance);
-            ventaRepository.save(venta);
+        if (pay > venta.getBalance()) {
+            throw new CustomException("El pago no puede superar el balance");
         }
+        Double totalPay = venta.getPay() + pay;
+        Double balance = venta.getTotal() - totalPay;
+        venta.setPay(totalPay);
+        venta.setBalance(balance);
+        ventaRepository.save(venta);
     }
 }
