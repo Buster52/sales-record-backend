69778aa9958ee550e4ac326be5bbe464ff86bfe6
buster <e.gonzalez@criptext.com>
2021-07-29 13:53:38 -0500

    refactor(exceptions): add property code

--- src/main/java/com/buster/backend/exceptions/AlreadyExistsException.java
+++ src/main/java/com/buster/backend/exceptions/AlreadyExistsException.java
@@ -1,7 +1,20 @@
 package com.buster.backend.exceptions;
 
 public class AlreadyExistsException extends RuntimeException {
+  private Integer code;
+
+  public AlreadyExistsException(){}
+
     public AlreadyExistsException(String message) {
         super(message);
+	this.code = 400;
+    }
+
+    public void setCode(Integer code) {
+      this.code = code;
+    }
+
+    public Integer getCode() {
+      return code;
     }
 }
--- src/main/java/com/buster/backend/exceptions/NotFoundException.java
+++ src/main/java/com/buster/backend/exceptions/NotFoundException.java
@@ -1,12 +1,23 @@
 package com.buster.backend.exceptions;
 
 public class NotFoundException extends RuntimeException {
+    
+    private Integer code;
 
     public NotFoundException(String message) {
         super(message);
+	this.code=404;
     }
 
     public NotFoundException() {
 
+    }
+
+    public void setCode(Integer code) {
+      this.code = code;
+    }
+
+    public Integer getCode() {
+      return code;
     }
 }
